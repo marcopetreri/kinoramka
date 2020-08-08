ad501f49f485be8e1a51c0ac1a820e81035f4920
Marco Petreri <marco.petreri@bitrock.it>
2020-08-08 17:35:02 +0200

    chore: added favicon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `kinoramka`,
+        name: `KINO RAMKA`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#cb0000`,
+        theme_color: `#cb0000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
